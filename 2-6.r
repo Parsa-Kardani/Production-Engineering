f53fafb79ee1ab328309f31aaec2ae4da6efac32
Parsa-Kardani <156613690+Parsa-Kardani@users.noreply.github.com>
2024-01-15 00:41:38 +0330

    Add files via upload

---
+++ 2-6.r
@@ -0,0 +1,26 @@
+# Importing required libraries
+library("MASS")
+library("scipy")
+library(ggplot2)
+
+#' Calculate Ppc using Ahmed method
+#' 
+#' This function calculates Ppc using the Ahmed method.
+#' 
+#' @param gamma_g The gamma_g value
+#' @param yN2 The mole fraction of N2
+#' @param yCO2 The mole fraction of CO2
+#' @param yH2S The mole fraction of H2S
+#' @return The calculated Ppc
+calculate_Ppc_ahmed_method <- function(gamma_g, yN2, yCO2, yH2S) {
+  Ppc <- 678 - 50 * (gamma_g - 0.5) - 206.7 * yN2 + 440 * yCO2 + 606.7 * yH2S
+  return(Ppc)
+}
+
+gamma_g <- 0.65  # specific gravity of gas
+T <- 250  # temperature in F
+Ppc <- calculate_Ppc_ahmed_method(gamma_g, 0, 0, 0)
+Ppr <- seq(14, 8000) / Ppc
+
+
+plot(seq(14, 8000), Ppr, type = "l", xlab = "Pressure (psi)", ylab = "Ppr", main = "Pseudo-Reduced Pressure vs Pressure")
