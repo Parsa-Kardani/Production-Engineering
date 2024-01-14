1a9550c19357458fcb0a5cae2d68a3c3e440c9a0
Parsa-Kardani <156613690+Parsa-Kardani@users.noreply.github.com>
2024-01-15 00:43:25 +0330

    Add files via upload

---
+++ 2-9.r
@@ -0,0 +1,68 @@
+# Importing required libraries
+library("MASS")
+library("scipy")
+
+
+#' Calculate gamma_g using apparent molecular weight
+#' 
+#' This function calculates gamma_g using the apparent molecular weight.
+#' 
+#' @param apparent_molecular_weight The apparent molecular weight
+#' @return The calculated gamma_g
+calculate_gamma_g_using_apparent_molecular_weight <- function(apparent_molecular_weight) {
+  gamma_g <- apparent_molecular_weight / 28.97
+  return(gamma_g)
+}
+
+#' Calculate Ppc using Ahmed method
+#' 
+#' This function calculates Ppc using the Ahmed method.
+#' 
+#' @param gamma_g The gamma_g value
+#' @param yN2 The mole fraction of N2
+#' @param yCO2 The mole fraction of CO2
+#' @param yH2S The mole fraction of H2S
+#' @return The calculated Ppc
+calculate_Ppc_ahmed_method <- function(gamma_g, yN2, yCO2, yH2S) {
+  Ppc <- 678 - 50 * (gamma_g - 0.5) - 206.7 * yN2 + 440 * yCO2 + 606.7 * yH2S
+  return(Ppc)
+}
+
+#' Calculate Tpc using Ahmed method
+#' 
+#' This function calculates Tpc using the Ahmed method.
+#' 
+#' @param gamma_g The gamma_g value
+#' @param yN2 The mole fraction of N2
+#' @param yCO2 The mole fraction of CO2
+#' @param yH2S The mole fraction of H2S
+#' @return The calculated Tpc
+calculate_Tpc_ahmed_method <- function(gamma_g, yN2, yCO2, yH2S) {
+  Tpc <- 326 + 315.7 * (gamma_g - 0.5) - 240 * yN2 - 83.3 * yCO2 + 133.3 * yH2S
+  return(Tpc)
+}
+
+# Mole fraction of each component
+y <- c(0.755, 0.073, 0.011, 0.006, 0.002, 0.003, 0.008, 0.001, 0.001, 0.07, 0.05, 0.02)
+
+# Molecular weight of each component
+mWi <- c(16.04, 30.07, 44.10, 58.12, 58.12, 72.15, 72.15, 86.18, 114.23, 28.02, 44.01, 34.08)
+
+# Calculate the apparent molecular weight
+mWa <- sum(y * mWi)
+
+# Calculate the specific gravity of the gas
+gamma_g <- calculate_gamma_g_using_apparent_molecular_weight(mWa)  
+
+# Calculate the pseudo critical pressure of the gas
+Ppc <- calculate_Ppc_ahmed_method(gamma_g, y[10], y[11], y[12])  
+
+# Calculate the pseudo critical temperature of the gas
+Tpc <- calculate_Tpc_ahmed_method(gamma_g, y[10], y[11], y[12]) 
+
+# Print the results
+cat("The apparent molecular weight of the gas is: ", mWa, "\n")
+cat("The specific gravity of the gas is: ", gamma_g, "\n")
+cat("The pseudo critical pressure of the gas is: ", Ppc, " psia\n")
+cat("The pseudo critical temperature of the gas is: ", Tpc, " R\n")
+
